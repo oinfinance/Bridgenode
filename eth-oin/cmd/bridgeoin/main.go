cb2a8e1b0f9e923a5c5c7d6c816cb4eca23f3039
liqh <codex8080@163.com>
2020-09-10 23:47:40 -0700

    some client for eth api test.

--- eth-oin/cmd/bridgeoin/main.go
+++ eth-oin/cmd/bridgeoin/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"os"
+	"time"
 
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/rpc"
 )
@@ -14,7 +18,9 @@ func init() {
 }
 
 func main() {
-
+	test_eth_client()
+}
+func test_rpc_client() {
 	client, err := rpc.Dial("http://47.242.40.47:6666")
 	if err != nil {
 		log.Error("dial rpc error", "error", err)
@@ -31,4 +37,42 @@ func main() {
 	}
 
 	log.Info("client call result", "account", account[0], "balance", result)
+}
+
+func test_rpc_ctx_client() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := rpc.Dial("http://47.242.40.47:6666")
+	if err != nil {
+		log.Error("dial rpc error", "error", err)
+		return
+	}
+
+	type Block struct {
+		Number string
+	}
+	var lastBlock Block
+	err = client.CallContext(ctx, &lastBlock, "eth_getBlockByNumber", "latest", true)
+	if err != nil {
+		log.Error("client call error", "error", err)
+		return
+	}
+	log.Info("client call result", "lastBlock", lastBlock)
+}
+
+func test_eth_client() {
+	client, err := ethclient.Dial("http://47.242.40.47:6666")
+	if err != nil {
+		log.Error("dial eth error", "error", err)
+		return
+	}
+
+	header, err := client.HeaderByNumber(context.Background(), nil)
+	if err != nil {
+		log.Error("get eth error", "error", err)
+		return
+	}
+
+	fmt.Println(header.Number.String())
 }
