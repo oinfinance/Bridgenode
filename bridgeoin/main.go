5ebbc26bd85937caf0e33a55608b8e37f4968827
liqh <codex8080@163.com>
2020-09-27 01:32:55 -0700

    del ont

---
+++ bridgeoin/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/ethereum/go-ethereum/log"
+	"os"
+)
+
+func init() {
+	glogger := log.NewGlogHandler(log.StreamHandler(os.Stderr, log.TerminalFormat(false)))
+	glogger.Verbosity(log.Lvl(log.LvlInfo))
+	log.Root().SetHandler(glogger)
+}
+
+func main() {
+	bridge := NewBridge()
+	bridge.Start()
+	log.Info("service over")
+}
